316ca2afc2c5fa8fb34e73df1f40c4b1806ed04d
SHS <santosh12saha@gmail.com>
2025-05-05 17:56:22 +0530

    update sidebar.data

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -51,7 +51,7 @@
                 </li>
                 @endcan
 
-                @can('view-vendors')
+                @can('view-candidate-sourcing')
                 <li class="nav-item">
                     <a class="nav-link {{ request()->routeIs('candidate-sourcing.*') ? 'active' : '' }}" href="{{ route('candidate-sourcing.index') }}">
                         <i class="fas fa-users me-2"></i> Vendors Dashboard
