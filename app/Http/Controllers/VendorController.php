20802bf9fe060148e359a7c3f68a4553c7508319
SHS <santosh12saha@gmail.com>
2025-05-08 16:59:11 +0530

    Update VendorController to Include 'vendor' Role in Internal Points of Contact Retrieval

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -98,7 +98,7 @@ class VendorController extends Controller
     {
         $this->authorize('create', Vendor::class);
         
-        $internalPocs = User::whereIn('role', ['admin', 'poc', 'hod'])->get();
+        $internalPocs = User::whereIn('role', ['admin', 'poc', 'hod', 'vendor'])->get();
         
         return view('vendors.create', compact('internalPocs'));
     }
@@ -224,7 +224,7 @@ class VendorController extends Controller
     {
         $this->authorize('update', $vendor);
         
-        $internalPocs = User::whereIn('role', ['admin', 'poc', 'hod'])->get();
+        $internalPocs = User::whereIn('role', ['admin', 'poc', 'hod', 'vendor'])->get();
         
         return view('vendors.edit', compact('vendor', 'internalPocs'));
     }
