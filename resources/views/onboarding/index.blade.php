e50b2f92d394b03e1ca05117237f79f6ffdcbbea
SHS <santosh12saha@gmail.com>
2025-06-05 20:44:35 +0530

    update

--- resources/views/onboarding/index.blade.php
+++ resources/views/onboarding/index.blade.php
@@ -103,37 +103,11 @@
 @endsection
 
 @section('scripts')
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+
 
 <script>
 $(document).ready(function() {
-    // State variables to hold filter values
-    var selectedStartDate = null;
-    var selectedEndDate = null;
-    var selectedStatus = '';
-    var filtersApplied = false;
-
-    function cb(start, end) {
-        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-    }
-
-    $('#reportrange').daterangepicker({
-        autoUpdateInput: false, // Don't update the input field automatically
-        locale: { cancelLabel: 'Clear' }, // Add a clear button
-        ranges: {
-            'Today': [moment(), moment()],
-            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-            'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        }
-    }, cb);
-
-    // Update the date range display initially
-    $('#reportrange span').html('Select Date Range'); // Initial display
+    
 
     // Then Initialize DataTable
     var table = $('#onboardingTable').DataTable({
@@ -225,7 +199,7 @@ $(document).ready(function() {
 
     // Status filter change handler
     $('#statusFilter').change(function() {
-        selectedStatus = $(this).val();
+       selectedStatus = $(this).val();
         var dateRange = $('#reportrange').data('daterangepicker');
         selectedStartDate = dateRange.startDate.format('YYYY-MM-DD');
         selectedEndDate = dateRange.endDate.format('YYYY-MM-DD');
